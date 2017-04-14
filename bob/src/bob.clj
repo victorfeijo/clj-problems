99efebbb6060b982b44ac1f64f5a525a98507cee
Victor Feijo <victor.feijoa@gmail.com>
2017-04-14 18:59:00 -0300

    improve some more tests

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -1,9 +1,18 @@
 (ns bob)
 (require '[clojure.string :as str])
 
+(defn is-shout?
+  [text]
+  (and (= (str/upper-case text) text)
+       (nil? (re-matches #"[0-9]*$" text))))
+
+(defn is-question? [text] (= (last text) \?))
+
+(defn is-blank? [text] (some? (re-matches #"\s*$" text)))
+
 (defn response-for [text]
   (cond
-    (= (str/upper-case text) text) "Whoa, chill out!"
-    (= (last text) \?) "Sure."
-    (= (last text) nil) "Fine. Be that way!"
+    (is-question? text) "Sure."
+    (is-shout? text) "Whoa, chill out!"
+    (is-blank? text) "Fine. Be that way!"
     :else "Whatever."))
